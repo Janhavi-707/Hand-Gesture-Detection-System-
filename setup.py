ac67afd701c2bbb2ddf260f681cf0e39b2003b5d
Janhavi-707 <janhavis461@gmail.com>
2026-04-17 14:30:24 +0530

    setup file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Hand Gesture Detection System',
+    version='0.1',
+    author="Janhavi",
+    author_email="janhavis461@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
